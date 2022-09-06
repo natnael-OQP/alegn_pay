fcb8a93b73684fa927fb3c9ecc1447e8e8532772
natnael-OQP <makiya112545@gmail.com>
2022-09-06 15:49:59 +0300

    change color

--- lib/screen/introduction/components/IndexIndicator.dart
+++ lib/screen/introduction/components/IndexIndicator.dart
@@ -3,16 +3,20 @@ import 'package:flutter/material.dart';
 class IndexIndicator extends StatelessWidget {
   const IndexIndicator({
     Key? key,
+    required this.index,
   }) : super(key: key);
+
+  final int index;
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
-      children: const [
-        Indicator(),
-        Indicator(),
-        Indicator(),
+      children: [
+        Indicator(bgColor: index == 0),
+        Indicator(bgColor: index == 1),
+        Indicator(bgColor: index == 2),
       ],
     );
   }
@@ -21,20 +25,25 @@ class IndexIndicator extends StatelessWidget {
 class Indicator extends StatelessWidget {
   const Indicator({
     Key? key,
+    required this.bgColor,
   }) : super(key: key);
+
+  final bool bgColor;
 
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
 
     return Container(
-      margin: EdgeInsets.all(width * .015),
-      width: 8,
-      height: 8,
-      decoration: const BoxDecoration(
-        color: Colors.black,
-        borderRadius: BorderRadius.all(
-          Radius.circular(10),
+      child: Container(
+        width: 8,
+        height: 8,
+        margin: EdgeInsets.all(width * .015),
+        decoration: BoxDecoration(
+          color: bgColor ? Colors.blue : Colors.black,
+          borderRadius: const BorderRadius.all(
+            Radius.circular(10),
+          ),
         ),
       ),
     );
