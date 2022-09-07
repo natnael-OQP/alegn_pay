b2ecb3094628cbb21b0feb5f8a2370e85d8c844f
natnael-OQP <makiya112545@gmail.com>
2022-09-07 23:23:34 +0300

    created login account

---
+++ lib/screen/login/screen/login_account.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LOginAccount extends StatelessWidget {
+  const LOginAccount({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
