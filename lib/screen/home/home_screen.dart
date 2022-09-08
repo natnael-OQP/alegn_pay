45b965d9427292d4b1c3eea7c18a75661a365257
natnael-OQP <makiya112545@gmail.com>
2022-09-08 11:05:31 +0300

    create home screen

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,22 +6,50 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text("task app"),
+        title: Row(
+          children: [
+            Image.asset("assets/images/"),
+            const Text(
+              "ALEGN",
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const Text(
+              "PAY",
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+          ],
+        ),
         actions: [
           IconButton(
             onPressed: () {},
-            icon: const Icon(Icons.add),
+            icon: const Icon(
+              FluentSystemIcons.ic_fluent_channel_notifications_filled,
+              color: Colors.black,
+            ),
           ),
         ],
       ),
-      body: Column(
-        children: const [
+      body: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: width * .04,
+          vertical: width * .07,
+        ),
+        child: Column(children: const [
           Center(
             child: Text("tasks:"),
           ),
-        ],
+        ]),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
