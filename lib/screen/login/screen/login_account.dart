a4a2ac01b7379814377fedc1d3e2f87094eeb8b1
natnael-OQP <makiya112545@gmail.com>
2022-09-08 11:11:08 +0300

    login account completed

--- lib/screen/login/screen/login_account.dart
+++ lib/screen/login/screen/login_account.dart
@@ -1,10 +1,190 @@
+import 'package:alegn_pay/screen/login/components/input_field.dart';
+import 'package:alegn_pay/screen/login/screen/forgot_password.dart';
+import 'package:alegn_pay/screen/login/screen/personal_info.dart';
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
-class LOginAccount extends StatelessWidget {
-  const LOginAccount({super.key});
+class LoginAccount extends StatelessWidget {
+  const LoginAccount({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: const Icon(
+              FluentSystemIcons.ic_fluent_ios_arrow_left_filled,
+              color: Colors.black,
+            ),
+          ),
+        ),
+        body: Center(
+          child: Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: width * .05,
+              // vertical: width * .07,
+            ),
+            height: height * .7,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Login account",
+                      style: TextStyle(
+                        fontSize: 28,
+                        height: width * .0027,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const Text(
+                      "Please fill your login detail",
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    InputField(
+                      height: height,
+                      width: width,
+                      label: 'Full Name',
+                      hint: 'Enter Full Name',
+                    ),
+                    InputField(
+                      height: height,
+                      width: width,
+                      label: 'Phone Number',
+                      hint: 'Enter Phone Number',
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Checkbox(
+                          value: false,
+                          onChanged: (value) => true,
+                        ),
+                        Expanded(
+                          child: Container(
+                            padding: EdgeInsets.only(top: width * .01),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                const Text(
+                                  "Remember me",
+                                  style: TextStyle(
+                                    fontSize: 13,
+                                  ),
+                                ),
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).push(
+                                      MaterialPageRoute(
+                                        builder: (context) =>
+                                            const ForgotPassword(),
+                                      ),
+                                    );
+                                  },
+                                  child: const Text(
+                                    "Forgot Password",
+                                    style: TextStyle(
+                                      color: Colors.red,
+                                      fontSize: 13,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                Column(
+                  children: [
+                    Container(
+                      alignment: Alignment.center,
+                      child: SizedBox(
+                        width: width * .7,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => const ForgotPassword(),
+                              ),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: HexColor("#194BE7"),
+                            padding: EdgeInsets.symmetric(
+                              horizontal: width * .05,
+                              vertical: width * .04,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(width * .03),
+                            ),
+                          ),
+                          child: const Text(
+                            "NEXT",
+                            style: TextStyle(
+                              fontSize: 13,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: width * .01),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            "Don't have an account?",
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.black,
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) => const PersonalInfo(),
+                                ),
+                              );
+                            },
+                            child: Text(
+                              "Sign Up",
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w500,
+                                color: HexColor("#194BE7"),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
